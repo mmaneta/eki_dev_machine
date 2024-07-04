c97629109e897f6d66073ceb730701780a8cfdde
Marco Maneta <mpmaneta@gmail.com>
2024-07-04 01:40:24 -0700

    fix adding new project_tag in main. Bumped dev version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Utilities to launch, provision, and delete aws host servers.
 
 config = {
     'name': 'dev_machine',
-    'version': '0.0.1.dev0',
+    'version': '0.0.2.dev0',
     'author': 'Marco Maneta',
     'author_email': 'mmaneta@ekiconsult.com',
     'description': 'EKI development machine ',
