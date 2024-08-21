8708548634e6266ed89aa6617ace00b0a9b8e50d
Marco Maneta <mpmaneta@gmail.com>
2024-08-21 15:03:15 -0700

    removed egg info. updated code to read version from setup.py

--- src/eki_dev/__init__.py
+++ src/eki_dev/__init__.py
@@ -6,11 +6,11 @@ try:
     # Normalize case for Windows systems
     dist_loc = os.path.normcase(_dist.location)
     here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'src')):
+    if not here.startswith(os.path.join(dist_loc, 'dev_machine')):
         # not installed, but there is another version that *is*
         raise DistributionNotFound
 except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
+    __version__ = 'Package version not available'
 else:
     __version__ = _dist.version
 
