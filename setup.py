251fd7c0efb4f785dffd8b639b5f8ea4d7fac7a6
Marco Maneta <mpmaneta@gmail.com>
2024-12-26 22:20:46 -0800

    modified setup.py version for non dev branch

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Utilities to launch, provision, and delete aws host servers.
 
 config = {
     'name': 'dev_machine',
-    'version': '0.2.0dev',
+    'version': '0.2.0',
     'author': 'Marco Maneta',
     'author_email': 'mmaneta@ekiconsult.com',
     'description': 'EKI development machine ',
